ba1873572adf3e36598232a6ad65dcd71ad6d541
MohamedAlhosiny <mohamedalsiri110@gmail.com>
2024-11-09 08:26:38 +0200

    update foreach

--- git.php
+++ git.php
@@ -12,3 +12,16 @@ if ($i = 5) {
 } else {
     echo "I hate you";
 }
+echo "<br>";
+
+$arr = [1, 2, 3, 4, 5];
+
+print_r($arr);
+
+
+echo "<br>";
+
+
+foreach ($arr as $id => $hamada) {
+    echo " $id => $hamada" . "<br>";
+}
